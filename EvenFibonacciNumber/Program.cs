2f8754d79d537858303fca130cbf69be01bc9228
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-11-03 21:26:54 -0600

    Finished problem 2.

---
+++ EvenFibonacciNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿// This is the answer to https://projecteuler.net/problem=2
+
+int max = 4000000;
+
+int answer = GetSumOfEvenFibonacciNumbers(max);
+
+Console.WriteLine($"The sum of even fibonacci numbers below 4,000,000 is {answer}");
+
+int GetSumOfEvenFibonacciNumbers(int max)
+{
+    int sum = 0;
+    int current = 2;
+    int previous = 1;
+
+    while (current < max)
+    {
+        if (current % 2 == 0)
+        {
+            sum += current;
+        }
+
+        int tempCurrent = current;
+        current += previous;
+        previous = tempCurrent;
+    }
+
+    return sum;
+}
