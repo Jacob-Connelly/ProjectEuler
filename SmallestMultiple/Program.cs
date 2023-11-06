8561663bec5db456d1c70d2be09e2e6ec7b4a00e
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-11-05 21:25:37 -0700

    Finished problem 5 brute force

---
+++ SmallestMultiple/Program.cs
@@ -0,0 +1,26 @@
+﻿// Solution to https://projecteuler.net/problem=5
+
+int max = 20;
+Console.WriteLine($"Smallest multiple of 1 to {20} is {GetSmallestMultipleBruteForce(max)}");
+
+int GetSmallestMultipleBruteForce(int max){
+
+    int count = max;
+    while (true)
+    {
+        for (int i = 2; i <= max; i++)
+        {
+            if (count % i != 0)
+            {
+                break;
+            }
+
+            if (i == max)
+            {
+                return count;
+            }
+        }
+
+        count++;
+    }
+}
