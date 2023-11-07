9e36d796a50f4e4b9ece4d132ed89c39fe0253e2
Jacob Connelly <jacob.connelly2018@outlook.com>
2023-11-06 19:02:16 -0700

    Finished problem 6. Felt like even less than a 5% difficulty.

---
+++ SumSquareDifference/Program.cs
@@ -0,0 +1,19 @@
+﻿// Solution to https://projecteuler.net/problem=6
+
+int max = 100;
+
+Console.WriteLine($"The difference between the sum of the squares of the first {max} natural numbers and the square of the sum is {GetSumSquareDifference(max)}");
+
+double GetSumSquareDifference(int max)
+{
+    double sumOfSquares = 0;
+    double squareOfSums = 0;
+
+    for (int i = 1; i <= max; i++)
+    {
+        sumOfSquares += Math.Pow(i, 2);
+        squareOfSums += i;
+    }
+
+    return Math.Pow(squareOfSums, 2) - sumOfSquares;
+}
